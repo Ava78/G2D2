c17d472ce79b2332c5b651148868ed497a862a1a
ava <chanie1981@gmail.com>
2016-05-27 18:05:50 +0200

    Ajout d'un slug dans la table Document

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527154004) do
+ActiveRecord::Schema.define(version: 20160527160128) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
 
 end
