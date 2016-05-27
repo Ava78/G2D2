5bbcc35de3e1a4f66a2004e6475a14509631c014
ava <chanie1981@gmail.com>
2016-05-27 17:24:33 +0200

    création du ctrler pages avec méthode index. Création de la vue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def welcome
-  	render text: "Bienvenue sur la page"
-  end
-
-
-
-
-
-
-
-
-
-
+  
 end
