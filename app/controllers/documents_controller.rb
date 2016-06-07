99c34e46a0771fc5dfa802efd94f393caeeae681
ava <chanie1981@gmail.com>
2016-06-07 15:16:41 +0200

    MAJ CSS et des liens

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -5,7 +5,6 @@ class DocumentsController < ApplicationController
 
   def show
   	#nous devons vérifier que cela ne fonctionne que si un document existe ainsi que son slug. En cas de cours non-existant --> redirigé sur page d'accueil
-
   	@document = Document.find_by_slug(params[:slug])
   	if @document.nil?
   		redirect_to root_path
@@ -17,6 +16,7 @@ class DocumentsController < ApplicationController
     redirect_to new_admin_document_path
 
   end
+
 
 
 
